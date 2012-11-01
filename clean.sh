52893bf261e3d26e5ca23e5c723e034efeb3804e
Michael Cole <quickstart@michaelcole.com>
2012-11-01 10:58:29 -0500

    Bring back build.sh

---
+++ clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## NOTE: You do not need to do this if you want to *use* Quickstart.  
+#
+#  You can save yourself some time with the instructions here:
+#
+#  http://<url>
+
+
+## This script will build a Quickstart Development Environment
+#
+# The Quickstart 3.0 build environment is Ubuntu 12.04.
+# If you're not using Ubuntu 12.04, this may not work.
+#
+# Documentation: http://<url>
+
+# Build everything else
+. clean-box.sh Base
+. clean-box.sh Test
+. clean-box.sh Prod
+. clean-box.sh Dev
+
+
+
+
+
